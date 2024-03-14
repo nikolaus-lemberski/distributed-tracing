b3df26d968f608fe577f2818011e2d77be0e4a4a
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-03-14 16:44:24 +0100

    Exception handling

--- apps/app-a/src/main/java/com/lemberski/appa/App.java
+++ apps/app-a/src/main/java/com/lemberski/appa/App.java
@@ -1,7 +1,6 @@
 package com.lemberski.appa;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -14,18 +13,25 @@ public class App {
     private static final String PORT = System.getenv().getOrDefault("PORT", "8080");
     private static final String APP_B = System.getenv().getOrDefault("APP_B", "http://localhost:8081");
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         Javalin.create().get("/", ctx -> {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(APP_B))
-                    .GET()
-                    .build();
+            String appB;
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(APP_B))
+                        .GET()
+                        .build();
 
-            HttpResponse<String> response = HttpClient
-                    .newHttpClient()
-                    .send(request, BodyHandlers.ofString());
+                HttpResponse<String> response = HttpClient
+                        .newHttpClient()
+                        .send(request, BodyHandlers.ofString());
 
-            ctx.result("App A <- " + response.body());
+                appB = response.body();
+            } catch (Exception e) {
+                appB = e.toString();
+            }
+
+            ctx.result("App A <- " + appB);
         }).start("0.0.0.0", Integer.parseInt(PORT));
     }
 }
