b874ba63cc655b870fbe2b3fd6bd51b9edc2d3fa
Nikolaus Lemberski <nikolaus@lemberski.com>
2024-03-18 19:13:01 +0100

    tag and push

--- apps/tag-and-push.sh
+++ apps/tag-and-push.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-export QUAY_USER=nlembers
 
-echo "## Tagging and pushing distributed-tracing-app-a"
-podman tag distributed-tracing-app-a:latest quay.io/$QUAY_USER/distributed-tracing-app-a:latest
-podman push quay.io/$QUAY_USER/distributed-tracing-app-a:latest
-echo "## distributed-tracing-app-a successfully pushed to quay.io"
+if [[ $QUAY_USER ]]
+then
+    echo "## Tagging and pushing distributed-tracing-app-a"
+    podman tag distributed-tracing-app-a:latest quay.io/$QUAY_USER/distributed-tracing-app-a:latest
+    podman push quay.io/$QUAY_USER/distributed-tracing-app-a:latest
+    echo "## distributed-tracing-app-a successfully pushed to quay.io"
 
-echo "## Tagging and pushing distributed-tracing-app-b"
-podman tag distributed-tracing-app-b:latest quay.io/$QUAY_USER/distributed-tracing-app-b:latest
-podman push quay.io/$QUAY_USER/distributed-tracing-app-b:latest
-echo "## distributed-tracing-app-b successfully pushed to quay.io"
+    echo "## Tagging and pushing distributed-tracing-app-b"
+    podman tag distributed-tracing-app-b:latest quay.io/$QUAY_USER/distributed-tracing-app-b:latest
+    podman push quay.io/$QUAY_USER/distributed-tracing-app-b:latest
+    echo "## distributed-tracing-app-b successfully pushed to quay.io"
 
-echo "## Tagging and pushing distributed-tracing-app-c"
-podman tag distributed-tracing-app-c:latest quay.io/$QUAY_USER/distributed-tracing-app-c:latest
-podman push quay.io/$QUAY_USER/distributed-tracing-app-c:latest
-echo "## distributed-tracing-app-c successfully pushed to quay.io"
+    echo "## Tagging and pushing distributed-tracing-app-c"
+    podman tag distributed-tracing-app-c:latest quay.io/$QUAY_USER/distributed-tracing-app-c:latest
+    podman push quay.io/$QUAY_USER/distributed-tracing-app-c:latest
+    echo "## distributed-tracing-app-c successfully pushed to quay.io"
 
-echo "DONE"
+    echo "DONE"
+else
+    echo "Set your quay.io username as QUAY_USER environment variable"
+fi
